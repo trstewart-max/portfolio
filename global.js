6d44e22eb58b08164808b83a1435245bbe36c5ad
Triton Stewart <trstewart@ucsd.edu>
2026-04-17 19:25:37 -0700

    final

--- global.js
+++ global.js
@@ -40,3 +40,49 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+`
+);
+
+let select = document.querySelector(".color-scheme select");
+
+if (localStorage.colorScheme) {
+  document.documentElement.style.setProperty("color-scheme", localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
+
+select.addEventListener("input", function (event) {
+  let value = event.target.value;
+  document.documentElement.style.setProperty("color-scheme", value);
+  localStorage.colorScheme = value;
+});
+
+
+
+let form = document.querySelector("form");
+
+form?.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+  let url = form.action + "?";
+  let params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${name}=${encodeURIComponent(value)}`);
+  }
+
+  url += params.join("&");
+  location.href = url;
+});
